ae7cfb76f51ddc851be2326578f11069ad9ef31b
Erik Weilow <nissehutt@gmail.com>
2015-06-02 15:18:12 +0200

    module.exports added

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports.config = require("./modules/config.js");
+module.exports.repository = require("./modules/repository.js");
+module.exports.node = require("./modules/node/node_controller.js");
+module.exports.master = require("./modules/master/master_controller.js");
